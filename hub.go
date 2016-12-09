64050dede46e427a7c8f01d3d99ff2c2c6d6f7d3
leoderoko <leoderoko@gmail.com>
2016-12-09 16:33:49 +0500

    Fix logs

--- hub.go
+++ hub.go
@@ -38,6 +38,7 @@ func (h *Hub) Log(jobID, message string) error {
 		JobID:     jobID,
 		Message:   message,
 	}
+	logrus.Infof("%s: %s", jobID, message)
 	return h.redis.Send(peskar.JobLogChannel, l)
 }
 
@@ -49,31 +50,39 @@ func (h *Hub) SuccessReceived(result []byte) error {
 	if job.State != "finished" {
 		return nil
 	}
+
 	h.Log(job.ID, "Got a job")
-	logrus.Infof("%s: Got a new job", job.ID)
 	p := job.Directory()
 	movieTarball := filepath.Join(h.config.ResultDir, p+".tar")
 	if _, err := os.Stat(movieTarball); os.IsNotExist(err) {
 		h.Log(job.ID, fmt.Sprintf("Error: %v", err))
 		return err
 	}
+
+	h.Log(job.ID, "Parsing and creating HTML page...")
 	err := SaveAsHTML(job, h.config.TemplateDir, h.config.ResultDir)
 	if err != nil {
 		h.Log(job.ID, fmt.Sprintf("Error: %v", err))
 		return err
 	}
 	h.Log(job.ID, "HTML page created")
-	logrus.Infof("%s: Unpacking...", job.ID)
+
+	h.Log(job.ID, "Extracting files from an archive...")
 	err = Untar(movieTarball, path.Join(h.config.ResultDir, p))
 	if err != nil {
-		h.Log(job.ID, fmt.Sprintf("Error: %v", err))
+		h.Log(job.ID, fmt.Sprintf("Error extracting: %v", err))
 		return err
 	}
-	h.Log(job.ID, "Tarball unarchived")
+	h.Log(job.ID, "Files extracted")
+
 	if err := os.Remove(movieTarball); err != nil {
-		h.Log(job.ID, fmt.Sprintf("Can't delete files: %v", err))
+		h.Log(job.ID, fmt.Sprintf("Can't delete archive: %v", err))
+	} else {
+		h.Log(job.ID, "Archive deleted")
 	}
-	logrus.Infof("%s: All done", job.ID)
+
+	h.Log(job.ID, "Done")
+
 	return nil
 }
 
